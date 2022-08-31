60a5194fc0907d5ce86b665c1f9c7590a0dd8127
Nathan Pizolito <108841167+pizolito@users.noreply.github.com>
2022-08-30 20:27:20 -0400

    Add files via upload

---
+++ Ex1/mainBola.py
@@ -0,0 +1,18 @@
+from CLASS.BOLA.bola import bola
+
+
+
+c = input("Digite a cor:")
+c.mostraCor()
+c = input("Digite a nova cor:")
+c.mostraCor()
+
+ci = float(input("Digite a circuferencia:"))
+ci.mostraCircuferencia()
+ci = float(input("Digite a nova Circuferencia:"))
+ci.mostraCircuferencia()
+
+m = input("Digite o material:")
+m.mostraMaterial()
+m = input("Digite o novoMaterial:")
+m.mostraMaterial()
